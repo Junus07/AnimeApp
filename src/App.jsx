4e9bd4c705997510f15a0241413ff8cff3463482
dzemil5 <dzemil.sahovic@gmail.com>
2024-09-12 00:38:30 +0200

    Odradjen footer, namesten routing ka drugim stranicama

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import Home from './components/Pages/Home/Home';
 import About from './components/Pages/About/About';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Sidebar />
+      <Footer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
