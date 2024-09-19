fb86a890921e20cea9bb22bed789c47edce6a5e0
dzemil5 <dzemil.sahovic@gmail.com>
2024-09-19 17:35:29 +0200

    Ispravljene greske, izmenjen CSS

--- src/components/Pages/Anime/Anime.jsx
+++ src/components/Pages/Anime/Anime.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import FetchAnime from "../../FetchAnime/FetchAnime";
 import SearchBar from "../../Search Bar/searchBar";
 import CategorySearch from "../../CategorySearch/categorySearch";
-import "./Anime.css";
 
 const Anime = () => {
 
